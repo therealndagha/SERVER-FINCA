4b524bf5705fcefd27244dea565d2e544001c5ad
Ndagha Kang'oma <ndaghakangoma@gmail.com>
2025-09-13 21:37:26 +0200

    Add second commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import customerRoutes from './routes/customer-routes.js';
 import staffRoutes from './routes/staff-routes.js';
-
+import branchRoutes from './routes/branch-routes.js';
+import groupRoutes from './routes/group-routes.js';
+import loanProductRoutes from './routes/loanProducts-routes.js';
 
 
 dotenv.config();
@@ -14,17 +16,23 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+     origin: 'http://localhost:5173', 
+    credentials: true}  
+));
 app.use(cookieParser());
 
-app.use('/api', (req, res)=>{
+app.get('/api', (req, res)=>{
     res.status(200).json({
         success: true,
         message: 'API is running...'
     })
 });
 
+app.use('/api/branch', branchRoutes)
 app.use('/api/customer',customerRoutes );
 app.use('/api/staff', staffRoutes);
+app.use('/api/group', groupRoutes);
+app.use('/api/loanproducts', loanProductRoutes)
 
 app.listen(PORT, ()=>console.log(`server is listening on port: ${PORT}`));
--- src/routes/customer-routes.js
+++ src/routes/customer-routes.js
@@ -1,10 +1,13 @@
 
 import express from 'express';
-import { getAllCustomers } from '../controllers/customer-controllers.js';
+import { addCustomers, deleteCustomer, getAllCustomers, updateCustomer } from '../controllers/customer-controllers.js';
 
 const router = express.Router();
 
 router.get('/', getAllCustomers);
+router.post('/', addCustomers);
+router.put('/:id', updateCustomer);
+router.delete('/:id', deleteCustomer);
 
 
 
